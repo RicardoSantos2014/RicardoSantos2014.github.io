2e9508eef8b4fb8e52f0e95a2977a2a88add6008
Ricardo Santos <ricardo.c.r.santos@gmail.com>
2020-10-21 01:14:20 -0300

    COMMIT 20.10.21 - Formulario feito
     - Criado o arquivo base index.html com os campos e formularios;
     - Criado o comportamento basico do JavaScript para a insercao de personagens;
     - Criado o CSS necessario para deixar a lista de jogadores e inimigos bonitinha.

---
+++ index.js
@@ -0,0 +1,61 @@
+var personagem = {
+    nome: "",
+    ataque: 0,
+    dano: 0,
+};
+var party = [];
+var inimigos = [];
+
+// document.onload = () => {
+//     button = document.getElementById("btnAddPartyMember").addEventListener('click', addPlayerToPartyFunction);
+//     partyList = document.getElementById("personagensDaParty");
+//     nomePersonagem = document.getElementById("nomePersonagem");
+//     ataquePersonagem = document.getElementById("ataquePersonagem");
+// }
+
+// function addPlayerToPartyFunction() {
+//     partyList.innerHTML = `<div> ${nomePersonagem.value} </div>`;
+// }
+
+$(document).ready(function(){
+    $('#nomePersonagem').val("");
+    $('#ataquePersonagem').val("");
+    $('#danoPersonagem').val("");
+
+    $("#btnAddPartyMember").click(() => {
+        if ($('#nomePersonagem').val() != "" )
+        {
+            personagem = {
+                nome: $('#nomePersonagem').val(),
+                ataque: $('#ataquePersonagem').val(),
+                dano: $('#danoPersonagem').val(),
+            };
+            party.push(personagem);
+            $(`<p> <div class="divNome"> ${personagem.nome} </div>
+                    <div class="divAtaque"> ${personagem.ataque} </div>
+                    <div class="divDano"> ${personagem.dano} </div> </p>`).appendTo('#personagensDaParty');
+            $('#nomePersonagem').val("");
+            $('#ataquePersonagem').val("");
+            $('#danoPersonagem').val("");
+        }
+    });
+
+    $("#btnAddInimigo").click(() => {
+        if ($('#nomeInimigo').val() != "" )
+        {
+            personagem = {
+                nome: $('#nomeInimigo').val(),
+                ataque: $('#ataqueInimigo').val(),
+                dano: $('#danoInimigo').val(),
+            };
+            inimigos.push(personagem);
+            $(`<p> <div class="divNome"> ${personagem.nome} </div>
+                    <div class="divAtaque"> ${personagem.ataque} </div>
+                    <div class="divDano"> ${personagem.dano} </div> </p>`).appendTo('#inimigosDaParty');
+            $('#nomeInimigo').val("");
+            $('#ataqueInimigo').val("");
+            $('#danoInimigo').val("");
+        }
+    });
+
+  });
